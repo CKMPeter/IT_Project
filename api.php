ec13247cb63a3be8cafa0d0be1057b6c9e6525ac
CKMPeter <126633396+CKMPeter@users.noreply.github.com>
2024-05-08 02:52:28 +0700

    add Login.html and working on Login DB in api

--- api.php
+++ api.php
@@ -1,13 +1,13 @@
 <?php
-
 session_start();
 require 'functions.php';
 
 $info = [];
 $info['success'] = false;
-$info['LOGGED_IN'] = is_logged_in();
+$info['LOGGED_IN'] = false;
+$info['data_type'] = $_POST['data_type'] ?? '';
 
-if(!$info['LOGGED_IN'])
+if(!$info['LOGGED_IN'] && $info['data_type'] != 'user_signup' && $info['data_type'] != 'user_login')
 {
     echo json_encode($info);
     die;
@@ -15,15 +15,13 @@ if(!$info['LOGGED_IN'])
 
 function is_logged_in(){
 
-        if(!empty($_SESSION['MY_DRIVE_USER']) && is_array($_SESSION['MY_DRIVE_USER']))
+    if(!empty($_SESSION['MY_DRIVE_USER']) && is_array($_SESSION['MY_DRIVE_USER']))
         return true;
-
     return false;
 }
 
 if($_SERVER['REQUEST_METHOD'] == "POST" && !empty($_POST['data_type']))
 {
-    $info['data_type'] = $_POST['data_type'];
 
     if($_POST['data_type'] == "upload_files")
     {
@@ -50,7 +48,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST" && !empty($_POST['data_type']))
             $date_updated = date("Y-m-d H:i:s");
             $file_name = $file['name'];
             $file_path = $destination;
-            $file_size = filezise($destination);
+            $file_size = filesize($destination);
             //sepcify the user
             $user_id = 0;
 
@@ -60,17 +58,79 @@ if($_SERVER['REQUEST_METHOD'] == "POST" && !empty($_POST['data_type']))
 
             $info['success'] = true;
         }
-    }else{
-        if($_POST['data_type'] == "get_files")
-        {    
-            $query = "select * from mydrive order by id desc limit 30";
-            $rows = query($query);
+    }else
+    if($_POST['data_type'] == "get_files")
+    {   
+        $mode = $_POST['mode'];
+
+        switch ($mode) {
+            case 'MY DRIVE':
+                $query = "select * from mydrive order by id desc limit 30";
+                break;
+
+            case 'FAVORITES':
+                $query = "select * from mydrive where favorite = 1 order by id desc limit 30";
+                break;
+
+            case 'RECENT':
+                $query = "select * from mydrive order by date_updated desc limit 30";
+                break;
+
+            case 'TRASH':
+                $query = "select * from mydrive where trash = 1 order by id desc limit 30";
+                break;
             
-            if($rows)
-            {
-                $info['rows'] = $rows;
-                $info['success'] = true;
+            default:
+                $query = "select * from mydrive order by id desc limit 30";
+                break;
+        }
+
+        $rows = query($query);
+        
+        if($rows)
+        {
+            foreach ($rows as $key => $row) {
+                $rows[$key]['icon'] = $icons[$row['file_type']] ?? '<i class = "bi bi-question-square-fill class 39"></i>';
+                $rows[$key]['date_created'] = get_date($row['date_created']);
+                $rows[$key]['date_updated'] = get_date($row['date_updated']);
+                $rows[$key]['file_size'] = round($row['file_size'] / (1024 * 1024)) . "Mb";
+
+                if( $rows[$key]['file_size'] == ")Mb")
+                {
+                    $rows[$key]['file_size']  = round($row['file_size'] / ( 1024)) . "Kb";
+
+                }
+                
             }
+
+            $info['rows'] = $rows;
+            $info['success'] = true;
+        }
+    }else
+    if($_POST['data_type'] == "user_signup")
+    {
+        //save to database
+        //attributions:
+        $email = addslashes($_POST['email']);
+        $user_name = addslashes($_POST['username']);
+        $password = addslashes($_POST['password']);
+        $date_created = date("Y-m-d H:i:s");
+        $date_updated = date("Y-m-d H:i:s");
+
+        //validate data
+        $errors = [];
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+        {
+            console.log($errors);
+            $errors['email'] = "Invalid email";
+        }
+        if(empty($errors))
+        {
+            $query = "INSERT INTO user (file_name, file_size, file_path, user_id, file_type, date_created, date_updated) 
+                    values ('$file_name', '$file_size', '$file_path', '$user_id', '$file_type', '$date_created', '$date_updated')";
+            query($query);
+
+            $info['success'] = true;
         }
     }
 }
--- functions.php
+++ functions.php
@@ -1,6 +1,10 @@
 <?php
 
 $con = mysqli_connect('localhost', 'root', '', 'mydrive_db');
+$icons = [
+
+    'image/jpeg' => '<i class = "bi bi-card-image class_39"></i>',
+];
 
 function query($query)
 {
@@ -15,7 +19,13 @@ function query($query)
             while($row = mysqli_fetch_assoc($result)){
                 $res[] = $row;
             }
+            return $res;
         }
     }
     return false;
+}
+
+function get_date($date)
+{
+    return date("jS M Y H:i:s a", strtotime($date));
 }
